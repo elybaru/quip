ec38f8bce7cce4a66aa64db880947a9d96c52e57
Ely Karr <elykarr@gmail.com>
2022-07-28 12:13:33 -0700

    added Conversation serializer attributes

--- app/serializers/conversation_serializer.rb
+++ app/serializers/conversation_serializer.rb
@@ -1,3 +1,6 @@
 class ConversationSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :users, :messages
+  attribute :users do |conversation|
+    UserSerializer.new(conversation.users.uniq).serializable_hash
+  end
 end
