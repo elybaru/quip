57060bc5a21784b7fe65af7e53c9a7adfce6ef59
Ely Karr <elykarr@gmail.com>
2022-07-28 12:28:49 -0700

    added new methods in messages_channel for streaming conversation data

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,9 +1,19 @@
 class MessagesChannel < ApplicationCable::Channel
     def subscribed
-      stream_from 'messages'
+    #   stream_from 'messages'
   
-      ActionCable.server.broadcast('messages', { messages: Message.all })
+    #   ActionCable.server.broadcast('messages', { messages: Message.all })
+    stop_all_streams
+    conversation = Conversation.find(params[:room])
+    stream_for conversation
+    end
+
+    
+    def received(data)
+        MessagesChannel.broadcast_to(conversation, { conversation: conversation, users: conversation.users, messages: conversation.messages})
     end
   
-    def unsubscribed; end
+    def unsubscribed
+        stop_all_streams
+    end
   end
